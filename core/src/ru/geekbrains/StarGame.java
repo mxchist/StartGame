63c6c00430d2fd96add2de4951631ee5685c4497
gods.like.you@gmail.com <ilovetolive@inbox.ru>
2020-04-30 11:53:23 +0300

    It's like trainer example but doesn't work.

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,40 +1,12 @@
 package ru.geekbrains;
+import com.badlogic.gdx.Game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import ru.geekbrains.screen.MenuScreen;
 
-import javax.swing.plaf.synth.Region;
+public class StarGame extends Game {
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img, backgroundFox;
-	TextureRegion region;
-	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		backgroundFox = new Texture("background_fox.jpg");
-		region = new TextureRegion(backgroundFox);
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(region, 0, 0);
-		batch.draw(img, 320, 412, 100, 100);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		setScreen(new MenuScreen());
 	}
 }
---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+    Texture img, backgroundFox;
+    TextureRegion region;
+    private Vector2 pos;
+    private Vector2 motion;
+    private Vector2 touch;
+
+    protected SpriteBatch batch;
+
+    @Override
+    public void show() {
+        super.show();
+        img = new Texture("badlogic.jpg");
+        backgroundFox = new Texture("background_fox.jpg");
+        region = new TextureRegion(backgroundFox);
+        pos = new Vector2();
+        motion = new Vector2(0.9f,0.9f);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        pos.add(motion);
+        batch.begin();
+        batch.draw(region, 0, 0);
+        batch.draw(img, pos.x, pos.y, 100, 100);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        img.dispose();
+        super.dispose();
+    }
+
+
+}
